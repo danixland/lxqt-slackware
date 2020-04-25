b1415a29714c04507d04d3b8a84c9457f7bd67a6
danix <danix@danix.xyz>
2020-04-25 20:38:36 +0200

    fixing runtime errors in DOWNLOAD.sh and lxqt.SlackBuild

--- DOWNLOAD.sh
+++ DOWNLOAD.sh
@@ -26,7 +26,7 @@ $GIT submodule init || echo "error initializing submodules" >> $ERROR_LOG
 $GIT submodule update --remote --rebase || echo "error during checkout of submodules" >> $ERROR_LOG
 
 # we grab the module's version numbers before deleting everything git related
-sh GRAB_VERSION.sh $SRCDIR
+sh ${CWD}/GRAB_VERSION.sh $SRCDIR
 
 # we are done with git, so let's cleanup from all the unwanted git folders and files
 find . -name '.git*' -mindepth 1 -exec rm -rfv '{}' \; || echo "error during removal of git files from source" >> $ERROR_LOG
